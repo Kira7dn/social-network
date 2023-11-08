2596d9306d9ae5ac4907ff1839fa7d1b688d6843
Kira7dn <89310517+Kira7dn@users.noreply.github.com>
2023-11-08 20:18:53 +0700

    change dropdown to menubar to fix scrollbar bug

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -3,38 +3,34 @@
 import * as React from "react";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-
-import { Button } from "@/components/ui/button";
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
 
 export function ModeToggle() {
   const { setTheme } = useTheme();
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
+    <Menubar>
+      <MenubarMenu>
+        <MenubarTrigger className="cursor-pointer">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+        </MenubarTrigger>
+        <MenubarContent>
+          <MenubarItem onClick={() => setTheme("light")}>Light</MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem onClick={() => setTheme("dark")}>Dark</MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem onClick={() => setTheme("system")}>System</MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+    </Menubar>
   );
 }
--- components/sections/UserItem.tsx
+++ components/sections/UserItem.tsx
@@ -4,64 +4,68 @@ import { ChevronsLeftRight } from "lucide-react";
 import { useUser, SignOutButton } from "@clerk/clerk-react";
 
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
+
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarTrigger,
+} from "../ui/menubar";
 
 export const UserItem = () => {
   const { user } = useUser();
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <div
-          role="button"
-          className="flex items-center text-sm p-3 w-full hover:bg-primary/5 rounded-lg"
-        >
-          <div className="gap-x-2 flex items-center max-w-[200px]">
-            <Avatar className="h-5 w-5">
-              <AvatarImage src={user?.imageUrl} />
-            </Avatar>
-            <span className="text-start font-medium line-clamp-1">
-              {user?.fullName}
-            </span>
-          </div>
-          <ChevronsLeftRight className="rotate-90 ml-2 text-muted-foreground h-4 w-4" />
-        </div>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent
-        className="w-80"
-        align="center"
-        alignOffset={11}
-        forceMount
-      >
-        <div className="flex flex-col space-y-4 p-2 z-10">
-          <p className="text-xs font-medium leading-none text-muted-foreground">
-            {user?.emailAddresses[0].emailAddress}
-          </p>
-          <div className="flex items-center gap-x-2">
-            <div>
+    <Menubar className="border-transparent px-2">
+      <MenubarMenu>
+        <MenubarTrigger className="cursor-pointer py-0 px-0 ">
+          <div
+            role="button"
+            className="flex items-center text-sm w-full h-full rounded-lg"
+          >
+            <div className="w-full h-full gap-x-2 flex items-center max-w-[200px]">
               <Avatar className="h-8 w-8">
                 <AvatarImage src={user?.imageUrl} />
               </Avatar>
+              <span className="text-start font-medium line-clamp-1">
+                {user?.fullName}
+              </span>
             </div>
-            <div className="space-y-1">
-              <p className="text-sm line-clamp-2">{user?.fullName}</p>
+            <ChevronsLeftRight className="rotate-90 ml-2 text-muted-foreground h-4 w-4" />
+          </div>
+        </MenubarTrigger>
+        <MenubarContent
+          className="w-80"
+          align="center"
+          alignOffset={11}
+          forceMount
+        >
+          <div className="flex flex-col space-y-4 p-2 z-10">
+            <p className="text-xs font-medium leading-none text-muted-foreground">
+              {user?.emailAddresses[0].emailAddress}
+            </p>
+            <div className="flex items-center gap-x-2">
+              <div>
+                <Avatar className="h-12 w-12">
+                  <AvatarImage src={user?.imageUrl} />
+                </Avatar>
+              </div>
+              <div className="space-y-1">
+                <p className="text-sm line-clamp-2">{user?.fullName}</p>
+              </div>
             </div>
           </div>
-        </div>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem
-          asChild
-          className="w-full cursor-pointer text-muted-foreground"
-        >
-          <SignOutButton>Log out</SignOutButton>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+          <MenubarSeparator />
+          <MenubarItem
+            asChild
+            className="w-full cursor-pointer text-muted-foreground"
+          >
+            <SignOutButton>Log out</SignOutButton>
+          </MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+    </Menubar>
   );
 };
